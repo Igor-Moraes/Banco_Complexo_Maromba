d901fdf93ac60b773ca620c60add5671ebe8bdd2
Igor Moraes <igorcdemoraes@gmail.com>
2026-09-17 16:30:41 -0300

    fix: arruma id das tabelas do schema contabil.

--- scripts/contabil.sql/complexo_contabil.sql
+++ scripts/contabil.sql/complexo_contabil.sql
@@ -4,23 +4,23 @@ CREATE TYPE contabil.tipo_conta_enum as ENUM('ATIVO','PASSIVO','PATRIMONIO_LIQUI
 CREATE TYPE contabil.natureza_conta_enum as ENUM('DEVEDORA','CREDORA');
 
 CREATE TABLE contabil.plano_contas(
-	id_lancamentos serial,
+	id_plano_contas serial,
 	codigo varchar(40) not null,
 	nome_conta varchar(255) not null,
 	tipo_conta contabil.tipo_conta_enum not null,
 	natureza_conta contabil.natureza_conta_enum not null DEFAULT 'DEVEDORA',
 	valor NUMERIC (10,2) NOT NULL ,
-	CONSTRAINT pk_plano_contas PRIMARY KEY(id)
+	CONSTRAINT pk_plano_contas PRIMARY KEY(id_plano_contas)
 );
 
 CREATE TABLE contabil.lancamentos(
 
-	id_plano_contas serial,
+	id_lancamento serial,
 	data_lancamento date not null,
 	historico varchar(255),
 	conta_debito_id integer,
 	conta_credito_id integer,
 	CONSTRAINT pk_lancamentos PRIMARY KEY(id),
-	CONSTRAINT fk_conta_debito FOREIGN KEY(conta_debito_id) REFERENCES contabil.plano_contas(id),
-	CONSTRAINT fk_conta_credito FOREIGN KEY(conta_credito_id) REFERENCES contabil.plano_contas(id)
+	CONSTRAINT fk_conta_debito FOREIGN KEY(conta_debito_id) REFERENCES contabil.plano_contas(id_plano_contas),
+	CONSTRAINT fk_conta_credito FOREIGN KEY(conta_credito_id) REFERENCES contabil.plano_contas(id_plano_contas)
 );
